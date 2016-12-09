817b7b8367a267ef5c7a73fc8aaf0c909a73e764
adam <athornton@gmail.com>
2016-12-08 19:47:48 -0700

    Initial commit

---
+++ example.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""Test Bitly OAuth2 Proxy session"""
+from __future__ import print_function
+import os
+from Session import Session as bses
+
+
+def main():
+    """Do the thing"""
+    username = os.environ["GITHUB_USERNAME"]
+    password = os.environ["GITHUB_PASSWORD"]
+    # Unless you have an LSST account you'd want to change these
+    baseurl = "https://logging.lsst.codes/oauth2/start"
+    authpage = "https://logging.lsst.codes/app/kibana"
+    session = bses(oauth2_username=username,
+                   oauth2_password=password,
+                   authentication_base_url=baseurl)
+    resp = session.get(authpage)
+    print(resp)
+    if resp.status_code == 200:
+        print(resp.content)
+
+
+if __name__ == "__main__":
+    main()
